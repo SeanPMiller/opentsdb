62a181530646fdd3a3020a13584de1f2b5882476
SeanPMiller <unvexis@yahoo.com>
2026-06-30 22:50:58 +0000

    test(implementation/prometheus): PowerMock->Mockito migration and test fixes.

--- implementation/prometheus/src/main/java/net/opentsdb/data/prometheus/PromQLParser.java
+++ implementation/prometheus/src/main/java/net/opentsdb/data/prometheus/PromQLParser.java
@@ -41,10 +41,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.google.common.base.Strings;
-
-import jersey.repackaged.com.google.common.collect.Lists;
-import jersey.repackaged.com.google.common.collect.Maps;
-import jersey.repackaged.com.google.common.collect.Sets;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+import com.google.common.collect.Sets;
 import net.opentsdb.data.types.numeric.NumericType;
 import net.opentsdb.prometheus.grammar.PromQLLexer;
 import net.opentsdb.prometheus.grammar.PromQLParser.AddOpContext;
